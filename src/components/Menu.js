d4cdcec9d10d29449116aed6cdd60e9473df0df9
Gabriel Rak <grakzanchetta@gmail.com>
2022-05-29 21:53:03 -0300

    falta: comportamento correto de deletar tarefa e consertar o botao de hoje

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,37 +1,61 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import DiarioContext from "../contexts/DiarioContext";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import { useContext } from "react";
 
 export default function Menu(){
+
+    const { diario } = useContext(DiarioContext)
 
     return (
         <Container>
             <NavLink to={`/habitos`}><h1>Hábitos</h1></NavLink>
-            <NavLink to={`/hoje`}>Hoje</NavLink>
+            <NavLink to="/hoje">
+                <Botao>
+                <CircularProgressbar
+                    value={diario}
+                    text={"Hoje"}
+                    background={true}
+                    backgroundPadding={6}
+                    styles={buildStyles({
+                        backgroundColor:'#52B6FF',
+                        textColor:'#FFFFFF',
+                        textSize:'18px',
+                        pathColor:'#ffffff'
+                    })}
+                />
+                </Botao>
+            </NavLink>
             <NavLink to={`/historico`}><h1>Histórico</h1></NavLink>
         </ Container>
     )
 }
 
-const Container = styled.div`
-    width: 100vw;
-    height: 70px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    position: fixed;
-    bottom: 0px;
-    background-color: #ffffff;
-
-    h1{
-        font-size: 18px;
-        color:#52B6FF;
-    }
-
+const Botao = styled.div`
+    height: 91px;
+    width: 91px;
+    margin-bottom: 31px;
 `
 
+const Container = styled.div`
+    width: 100%;
+    height: 70px;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left:36px ;
+    padding-right: 36px;
+    background-color: #fff;
+    
+   h1{
+        font-size: 18px;
+        color:#52B6FF;
+    } 
+`
 const NavLink = styled(Link)`
   text-decoration: none;
 }
 `
-
-
